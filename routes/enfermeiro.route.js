0009c95d7d8da25e747ce8770c1dc3101375249e
KaylaneFreitas <144849129+KaylaneFreitas@users.noreply.github.com>
2024-06-05 21:39:00 -0300

    Update enfermeiro.route.js

--- routes/enfermeiro.route.js
+++ routes/enfermeiro.route.js
@@ -1,1 +1,18 @@
-
+module.exports = (app) => {
+const enfermeiro = require("../controllers/enfermeiro.controller.js" );
+var router = require("express").Router();
+// Rota para criar um enfermeiro
+router.post("/", enfermeiro.create);
+// Rota que retorna todos os enfermeiros
+router.get("/", enfermeiro.findAll);
+// Rota que retorna um enfermeiro pelo id
+router.get("/:id", enfermeiro.findOne);
+// Rota que atualiza um enfermeiro pelo id
+router.put("/:id", enfermeiro.update);
+// Rota para deletar um enfermeiro pelo id
+router.delete("/:id", enfermeiro.delete);
+// Rota para deletar todos os enfermeiros
+router.delete("/", enfermeiro.deleteAll);
+// A linha abaixo informa que todas essas rotas são encontradas após o /enfermeiro. Isto é, localhost:8080/enfermeiro/rota
+app.use("/enfermeiro", router);
+};
