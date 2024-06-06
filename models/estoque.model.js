2de0ef435128e26ae738f553eb961070f153e4d8
KaylaneFreitas <144849129+KaylaneFreitas@users.noreply.github.com>
2024-06-05 21:28:56 -0300

    Update estoque.model.js

--- models/estoque.model.js
+++ models/estoque.model.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, Sequelize) => {
     const Registros = sequelize.define( "registros", {
     QuantidadeDoada: { type: Sequelize.FLOAT },
     Tipo: { type: Sequelize.STRING },
+    Numero: { type: Sequelize.FLOAT },
     },
     { freezeTableName: true }
 );
