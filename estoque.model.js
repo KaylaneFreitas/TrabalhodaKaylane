1ff9be9486ff3b39e827dfc1f2454bdfdcf81284
KaylaneFreitas <144849129+KaylaneFreitas@users.noreply.github.com>
2024-06-05 21:50:50 -0300

    Delete estoque.model.js

--- estoque.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Registros = sequelize.define( "registro", {
-    QuantidadeDoada: { type: Sequelize.FLOAT },
-    Tipo: { type: Sequelize.STRING },
-    },
-    { freezeTableName: true }
-);
-return Registros;
-};
