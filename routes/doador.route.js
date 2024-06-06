d97f58cd9775095da1d9bfac3be0b7a39fd43c8b
KaylaneFreitas <144849129+KaylaneFreitas@users.noreply.github.com>
2024-06-05 21:35:49 -0300

    Update doador.route.js

--- routes/doador.route.js
+++ routes/doador.route.js
@@ -1,1 +1,18 @@
-
+module.exports = (app) => {
+const Doador = require("../controllers/doador.controller.js" );
+var router = require("express").Router();
+// Rota para criar um doador
+router.post("/", doador.create);
+// Rota que retorna todos os doadores
+router.get("/", doador.findAll);
+// Rota que retorna um doador pelo id
+router.get("/:id", doador.findOne);
+// Rota que atualiza um doador pelo id
+router.put("/:id", doador.update);
+// Rota para deletar um doador pelo id
+router.delete("/:id", doador.delete);
+// Rota para deletar todos os doadores
+router.delete("/", doador.deleteAll);
+// A linha abaixo informa que todas essas rotas são encontradas após o /doadores. Isto é, localhost:8080/doadores/rota
+app.use("/doadores", router);
+};
