792a5494b923069b34550561f840c4b2ea6e8037
KaylaneFreitas <144849129+KaylaneFreitas@users.noreply.github.com>
2024-06-05 21:09:41 -0300

    Update registros.controller.js

--- controllers/registros.controller.js
+++ controllers/registros.controller.js
@@ -2,31 +2,6 @@ const db = require("../models");
 const registros = db.registros;
 const Op = db.Sequelize.Op;
 
-// Cria e salva um novo registro
-exports.create = (req, res) => {
-};
-
-// Retorna todos os registros do banco de dados. Podemos passar um parâmetro e
-//filtrar
-exports.findAll = (req, res) => {
-};
-
-// Encontra uma doação pelo id
-exports.findOne = (req, res) => {
-};
-
-// Atualiza os dados de um registro
-exports.update = (req, res) => {
-};
-
-// Deleta uma doação pelo id
-exports.delete = (req, res) => {
-};
-
-// Deleta todas as doações do banco de dados
-exports.deleteAll = (req, res) => {
-};
-
 exports.create = (req, res) => {
     // Validar requisição. Aqui verificamos se algum parâmetro está vazio
     if (!req.body.nome) {
@@ -43,3 +18,117 @@ exports.create = (req, res) => {
     enfermeiroId: req.body.enfermeiroId,
     };
 }
+  // Salvar registro no banco de dados
+  Registros.create(registros)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro durante a criação do Doador.",
+      });
+    });
+};
+
+// Retorna todos os doadores do banco de dados. Podemos passar um parâmetro e filtrar
+exports.findAll = (req, res) => {
+  const nome = req.query.nome;
+  var condition = nome ? { nome: { [Op.iLike]: `%${nome}%` } } : null;
+
+  Doador.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro durante a procura pelo doador.",
+      });
+    });
+};
+
+// Encontra um doador pelo id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Doador.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Não é possível achar o doador com o id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Erro na busca pelo doador via id=" + id,
+      });
+    });
+};
+
+// Atualiza os dados do doador
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Doador.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Doador foi atualizado com sucesso.",
+        });
+      } else {
+        res.send({
+          message: `Não foi possível atualizar o doador com id=${id}. Talvez o doador não tenha sido encontrada ou req.body está vazio!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Erro em atualizar o doador via id=" + id,
+      });
+    });
+};
+
+// Deleta um doador pelo id
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Doador.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Doador foi deletada com sucesso!",
+        });
+      } else {
+        res.send({
+          message: `Não é possível deletar esse doador; Ele não foi encontrada!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Não é possível deletar o doador com id=" + id,
+      });
+    });
+};
+
+// Deleta todos os doadores do banco de dados
+exports.deleteAll = (req, res) => {
+  Doador.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} Doadores foram deletadas com sucesso!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro enquanto deletava os doadores.",
+      });
+    });
+};
